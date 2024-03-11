988b85877339b81137eaf83d14aa8555da981767
Jared <jared.comiso@98labs.com>
2024-03-11 18:13:01 +0800

    added nginx in docker

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,9 @@ RUN yarn
 COPY . .
 RUN yarn build
 
-EXPOSE 3000
-
-CMD ["yarn", "start"]
+#Stage 2
+FROM nginx:1.19.0
+WORKDIR /usr/share/nginx/html
+RUN rm -rf ./*
+COPY --from=builder /app/build .
+ENTRYPOINT ["nginx", "-g", "daemon off;"]
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Version 2.0 Kamagong Version
+          Version 69 Kamagong Version
         </p>
         <a
           className="App-link"
