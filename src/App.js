a11b9c9ce788a0d2c5dae6f9925ac148810eec18
Jared <jared.comiso@98labs.com>
2024-03-11 16:14:42 +0800

    update display

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload mo to.
+          Version 2.0 Kamagong Version
         </p>
         <a
           className="App-link"
